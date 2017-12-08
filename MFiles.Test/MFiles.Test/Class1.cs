9c4495354b35910ad002de24f8bc6828d8be39c0
qiuge <287803810@qq.com>
2017-12-08 22:17:02 +0800

    add amount

--- MFiles.Test/MFiles.Test/Class1.cs
+++ MFiles.Test/MFiles.Test/Class1.cs
@@ -14,5 +14,7 @@ namespace MFiles.Test
         public string Address { get; set; }
         public string Email { get; set; }
          public string Phone { get; set; }
+
+        public string Amount { get; set; }
     }
 }
